01381ad0a0b582b3fdc5d9192c9112af55aab03d
Ross Edfort <rossedfort@gmail.com>
2015-09-30 08:51:12 -0600

    finalized

--- spec/parsing_spec.rb
+++ spec/parsing_spec.rb
@@ -5,23 +5,23 @@ require 'pry'
 
 class ParsingUnitTest < Minitest::Test
   def test_parses_the_first_line
-    client = StringIO.new("POST /to_braille HTTP/1.1\r\n" +
-                       "Content-length: 10\r\n" +
-                       "\r\n" +
-                       "body")
+    client = StringIO.new("POST /to_braille HTTP/1.1\r\n" \
+                       "Content-length: 10\r\n" \
+                       "\r\n" \
+                       'body')
 
     env = HttpYeahYouKnowMe.parse_request(client)
-    assert_equal "HTTP/1.1", env['VERSION']
-    assert_equal "/to_braille", env['PATH_INFO']
-    assert_equal "POST", env['REQUEST_METHOD']
+    assert_equal 'HTTP/1.1', env['VERSION']
+    assert_equal '/to_braille', env['PATH_INFO']
+    assert_equal 'POST', env['REQUEST_METHOD']
   end
 
-  def test_it_parses_no_nore_than_it_needs_to
-    client = StringIO.new("POST /to_braille HTTP/1.1\r\n" +
-                          "Content-length: 10\r\n" +
-                          "\r\n" +
+  def test_it_parses_no_nore_than_it_needs_to_in_the_body
+    client = StringIO.new("POST /to_braille HTTP/1.1\r\n" \
+                          "Content-length: 10\r\n" \
+                          "\r\n" \
                           "0123456789This shit shouldn't show")
     env = HttpYeahYouKnowMe.parse_request(client)
-    assert_equal "0123456789", env['rack.input'].gets
+    assert_equal '0123456789', env['rack.input'].gets
   end
 end
